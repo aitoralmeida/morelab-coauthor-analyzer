a68d94959d9174d389d95d724b155485405aaa30
Aitor Almeida <aitoralmeida@gmail.com>
2013-04-18 12:06:26 +0200

    the analysis is written in csv files, divided the program in  functions

--- NetworkAnalyzer.py
+++ NetworkAnalyzer.py
@@ -9,101 +9,151 @@ Created on Tue Mar 05 16:42:59 2013
 import networkx as nx
 import matplotlib.pylab as plt
 from collections import OrderedDict
+import csv
 
-verbose = False
+verbose = True
+data_path = "./data/coauthors.txt"
 
-fh = open("./data/coauthors.txt", 'rb')
-G = nx.read_edgelist(fh)
-fh.close()
-
-#remove posible self loops
-G.remove_edges_from(G.selfloop_edges())
-
-#network info
-node_num = G.number_of_nodes()
-edge_num = G.number_of_edges()
-nodes = G.nodes()
-
-if verbose:
-    print "The loaded network has %s nodes and %s edges\r\n" % (node_num, edge_num)
-    print "The nodes of the network are:"
-    for n in nodes:
-        print n
-
-#simple network visualization 
-nx.draw(G)
-plt.savefig("./images/simpleNetwork.png")
-if verbose:
-    plt.show()
-
-#centrality analysis
-#degree centrality
-cent_degree = nx.degree_centrality(G)
-sorted_cent_degree = OrderedDict(sorted(cent_degree.items(), key=lambda t: t[1], reverse=True))
-if verbose:
-    print "\n\r*** Degree Centrality ***"
-    for d in sorted_cent_degree:
-        print "%s: %s" % (d, sorted_cent_degree[d])
-        
-#betweenness centrality
-cent_betweenness = nx.betweenness_centrality(G)
-sorted_cent_betweenness = OrderedDict(sorted(cent_betweenness.items(), key=lambda t: t[1], reverse=True))
-if verbose:
-    print "\n\r*** Betweenness Centrality ***"
-    for d in sorted_cent_betweenness:
-        print "%s: %s" % (d, sorted_cent_betweenness[d])
-
-#closeness centrality
-cent_closeness = nx.closeness_centrality(G)
-sorted_cent_closeness = OrderedDict(sorted(cent_closeness.items(), key=lambda t: t[1], reverse=True))
-if verbose:
-    print "\n\r*** Closeness Centrality ***"
-    for d in sorted_cent_closeness:
-        print "%s: %s" % (d, sorted_cent_closeness[d])
-
-#eigenvector centrality
-cent_eigenvector = nx.eigenvector_centrality(G)
-sorted_cent_eigenvector = OrderedDict(sorted(cent_eigenvector.items(), key=lambda t: t[1], reverse=True))
-if verbose:
-    print "\n\r*** Eigenvector Centrality ***"
-    for d in sorted_cent_eigenvector:
-        print "%s: %s" % (d, sorted_cent_eigenvector[d])
-        
-#pageRank
-#eigenvector centrality
-page_rank = nx.pagerank(G)
-sorted_page_rank = OrderedDict(sorted(page_rank.items(), key=lambda t: t[1], reverse=True))
-if verbose:
-    print "\n\r*** PageRank ***"
-    for d in sorted_page_rank:
-        print "%s: %s" % (d, sorted_page_rank[d])
-        
-#groups
-#cliques
-cliques = list(nx.find_cliques(G))
-if verbose:
-    for c in cliques:
-        print "\n\r*** Cliques ***"
-        print c
-
-#k-core
-#main
-core_main = nx.k_core(G)
-nx.draw(core_main)
-plt.savefig("./images/kCoreMain.png")
-if verbose:
-    print "\r\nk-Core: Main"
-    print core_main.nodes()
-    plt.show()
+def get_graph(path):
+    fh = open(path, 'rb')
+    G = nx.read_edgelist(fh)
+    fh.close()
+    #remove posible self loops
+    G.remove_edges_from(G.selfloop_edges())
+    return G
     
-#k = 4
-core_main = nx.k_core(G, k=4)
-nx.draw(core_main)
-plt.savefig("./images/kCore4.png")
-if verbose:
-    print "\r\nk-Core: 4"
-    print core_main.nodes()
-    plt.show()
-   
+        
+def write_csv_centralities(file_name, data):
+    with open(file_name, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        for d in data:
+            if verbose:
+                print "%s: %s" % (d, data[d])
+            writer.writerow([d, data[d]])
+            
+def write_csv_groups(file_name, groups):
+    with open(file_name, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        for e in groups:
+            if verbose:
+                print e
+            writer.writerow(e)
+            
+def write_csv_group(file_name, group):
+    with open(file_name, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow(group)
 
+def get_graph_info(G):
+    node_num = G.number_of_nodes()
+    edge_num = G.number_of_edges()
+    nodes = G.nodes()
+    
+    if verbose:
+        print "The loaded network has %s nodes and %s edges\r\n" % (node_num, edge_num)
+        print "The nodes of the network are:"
+        for n in nodes:
+            print n
+            
+    with open('./data/results/networkInfo.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow(['nodes',node_num])    
+        writer.writerow(['edges',edge_num])    
 
+def draw_graph(G):
+    nx.draw(G)
+    plt.savefig("./images/simpleNetwork.png")
+    if verbose:
+        plt.show()
+
+#**********CENTRALITIES***********
+  
+def calculate_degree_centrality(G):
+    cent_degree = nx.degree_centrality(G)
+    sorted_cent_degree = OrderedDict(sorted(cent_degree.items(), key=lambda t: t[1], reverse=True))
+    if verbose:
+        print "\n\r*** Degree Centrality ***"
+    write_csv_centralities('./data/results/degreeCent.csv', sorted_cent_degree)
+
+def calculate_betweenness_centrality(G):    
+    cent_betweenness = nx.betweenness_centrality(G)
+    sorted_cent_betweenness = OrderedDict(sorted(cent_betweenness.items(), key=lambda t: t[1], reverse=True))
+    if verbose:
+        print "\n\r*** Betweenness Centrality ***"
+    write_csv_centralities('./data/results/betweennessCent.csv', sorted_cent_betweenness)
+
+def calculate_closeness_centrality(G):   
+    cent_closeness = nx.closeness_centrality(G)
+    sorted_cent_closeness = OrderedDict(sorted(cent_closeness.items(), key=lambda t: t[1], reverse=True))
+    if verbose:
+        print "\n\r*** Closeness Centrality ***"
+    write_csv_centralities('./data/results/closenessCent.csv', sorted_cent_closeness)   
+
+def calculate_eigenvector_centrality(G):   
+    cent_eigenvector = nx.eigenvector_centrality(G)
+    sorted_cent_eigenvector = OrderedDict(sorted(cent_eigenvector.items(), key=lambda t: t[1], reverse=True))
+    if verbose:
+        print "\n\r*** Eigenvector Centrality ***"
+    write_csv_centralities('./data/results/eigenvectorCent.csv', sorted_cent_eigenvector) 
+        
+def calculate_pagerank(G):
+    page_rank = nx.pagerank(G)
+    sorted_page_rank = OrderedDict(sorted(page_rank.items(), key=lambda t: t[1], reverse=True))
+    if verbose:
+        print "\n\r*** PageRank ***"
+    write_csv_centralities('./data/results/pagerank.csv', sorted_page_rank)     
+    
+    
+#**********COMMUNITIES***********
+def calculate_cliques(G):
+    cliques = list(nx.find_cliques(G))
+    if verbose:
+        print "\n\r*** Cliques ***"
+    write_csv_groups('./data/results/cliques.csv', cliques)
+    
+def calculate_main_k_core(G):
+    core_main = nx.k_core(G)
+    nx.draw(core_main)
+    plt.savefig("./images/kCoreMain.png")
+    if verbose:
+        print "\r\nk-Core: Main"
+        print core_main.nodes()
+        plt.show()
+    write_csv_group('./data/results/mainKCore.csv', core_main.nodes())
+    
+def calculate_k_core(G, K):
+    core_k = nx.k_core(G, k=K)
+    nx.draw(core_k)
+    plt.savefig("./images/kCore" + str(K) + ".png")
+    if verbose:
+        print "\r\nk-Core: " + str(K)
+        print core_k.nodes()
+        plt.show()
+    write_csv_group('./data/results/kCore' + str(K) + '.csv', core_k.nodes())
+    
+def calculate_k_clique(G, K):
+    communities = nx.k_clique_communities(G, K)
+    if verbose:
+        print "k-cliques " + str(K)
+    write_csv_groups('./data/results/kClique' + str(K) + '.csv', communities)
+
+        
+    
+    
+print 'Analizing co-author data from ' + data_path
+G = get_graph(data_path)
+get_graph_info(G)
+draw_graph(G)
+
+#centralities
+calculate_degree_centrality(G)
+calculate_betweenness_centrality(G)
+calculate_closeness_centrality(G)
+calculate_closeness_centrality(G)
+calculate_pagerank(G)
+
+#communities
+calculate_cliques(G)
+calculate_main_k_core(G)
+calculate_k_core(G,4)
+calculate_k_clique(G, 2)
